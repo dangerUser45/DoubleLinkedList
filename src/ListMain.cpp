32fcb82de166e6493e019f724f9826485405e062
Danil <danya541@bk.ru>
2025-04-22 09:32:50 +0300

    add TODO;

--- src/ListMain.cpp
+++ src/ListMain.cpp
@@ -20,42 +20,6 @@ int main ()
     ListInsertAfter (&list, 400, &(list.node_array[2])); ListGraphDump (&list);
     ListInsertBelow (&list, -999, list.head); ListGraphDump (&list);
 
-//     ListInsertData (&list, &(list.node_array[0]), 51); ListGraphDump (&list);
-
-//     list_node* dest_find_data = 0;
-//     ListFindData (&list, 443, &dest_find_data);
-//
-//     size_t index = 0;
-//     ListFindIndex (&list, list.tail, &index);
-//
-//     list_node* dest_find_node = 0;
-//     ListFindNode (&list, index, &dest_find_node);
-//
-//     printf ("dest_find_data = %p\n"
-//             "index = %zu\n"
-//             "dest_find_node = %p\n", dest_find_data, index, dest_find_node);
-
-    //InsertAfter (&list, 600, &(list.node_array[0])); ListDump (&list);
-//     InsertTail (&list, 222); ListDump (&list);
-
-//     InsertHead (&list, 222);
-//     InsertHead (&list, 111);
-//
-//     //ListGraphDump();
-//
-//     InsertTail (&list, 1);
-//     InsertTail (&list, 2);
-//     InsertTail (&list, 3);
-//     InsertTail (&list, 4);
-//     InsertTail (&list, 5);
-//     InsertTail (&list, 6);
-//     InsertTail (&list, 7);
-//     InsertTail (&list, 8);
-//     InsertTail (&list, 9);
-//     InsertTail (&list, 10);
-//     InsertTail (&list, 11);
-//     InsertTail (&list, 12);
-
     DtorListCommon (&list);
 }
 //--------------------------------------------------------------
